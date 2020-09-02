5f3f2371f0e83e2770cddcdc9c5f653b63d04fd4
wannasakon <wannasakon.ph.61@ubu.ac.th>
2020-09-02 11:24:44 +0700

    Updates

---
+++ precache-manifest.93981ebb0f47794a12e68b5177255b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b05cefb59816bb2129d25b34746f984",
+    "url": "/-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ce007bfd982ff54ebc07",
+    "url": "/-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "6b10f922532e92bb6644",
+    "url": "/-tic-tac-toe/static/js/2.a5f382e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-tic-tac-toe/static/js/2.a5f382e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce007bfd982ff54ebc07",
+    "url": "/-tic-tac-toe/static/js/main.15237da6.chunk.js"
+  },
+  {
+    "revision": "ecd117df8abfde2a29c9",
+    "url": "/-tic-tac-toe/static/js/runtime-main.678f2231.js"
+  }
+]);
